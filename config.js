6148f87367ec5fc6d61d64d091394d6a6e03d687
Kaarstthenn-21 <kaarstthen.ancco@tecsup.edu.pe>
2022-05-24 14:40:56 -0500

    Add Package Dotenv for handle variables environment

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 module.exports = {
     api: {
-        port: process.env.HTTPS_PORT || process.env.HTTP_PORT,
+        port: process.env.HTTP_PORT || 3000,
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'kaarstthenn',
