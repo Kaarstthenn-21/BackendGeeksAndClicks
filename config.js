4e0942d30be6090d6c3b270bc6e5d754b81a748e
Kaarstthenn-21 <kaarstthen.ancco@tecsup.edu.pe>
2022-05-24 14:20:19 -0500

    New Feature & Connection Db

--- config.js
+++ config.js
@@ -6,16 +6,15 @@ module.exports = {
         secret: process.env.JWT_SECRET || 'kaarstthenn',
     },
     mysql: {
-        host: process.env.MYSQL_HOST || 'geeks-db.cjvsoudrdr7r.us-west-2.rds.amazonaws.com',        
+        host: process.env.MYSQL_HOST || 'geeks-db.cjvsoudrdr7r.us-west-2.rds.amazonaws.com',
         user: process.env.MYSQL_USER || 'admingeeks',
         password: process.env.MYSQL_PASS || 'geeks123',
-        database: process.env.MYSQL_DB || 'user',
-    cloudinary: {
-        cloud_name:'riacrdo2',
-        api_key:'724884842686881',
-        api_secret:'z7jsFVMQbrmnjiAArtsJ_XJoY_I',
-        secure:true
+        database: process.env.MYSQL_DB || 'user'
     },
-    
-
+    cloudinary: {
+        cloud_name: 'riacrdo2',
+        api_key: '724884842686881',
+        api_secret: 'z7jsFVMQbrmnjiAArtsJ_XJoY_I',
+        secure: true
+    },
 }
