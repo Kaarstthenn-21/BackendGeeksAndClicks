322d83c32e28cad5e5af88af222b7cdcca889431
Kaarstthenn-21 <kaarstthen.ancco@tecsup.edu.pe>
2022-05-24 18:43:50 -0500

    refactor env ip

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     api: {
         port: process.env.HTTPS_PORT || process.env.HTTP_PORT || 3000,
-        ip: process.env.ip || '127.0.0.1',
+        ip: process.env.ip,
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'kaarstthenn',
